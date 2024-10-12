6452dc1a1c530fb9cddcca098716d0f414ebcfa4
Hryhoriev Vladislav <wladislav101@gmail.com>
2024-10-12 17:21:19 +0300

    update

---
+++ prisma/seed.ts
@@ -0,0 +1,47 @@
+import { hashSync } from "bcrypt";
+import { prisma } from "./prisma-client";
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "User Test",
+        email: "user@test.com",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "USER",
+      },
+      {
+        fullName: "Admin Admin",
+        email: "admin@test.com",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "ADMIN",
+      },
+    ],
+  });
+}
+
+async function down() {
+  await prisma.user.deleteMany({});
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
